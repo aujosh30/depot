7467535e6a54a5218cf4e70d067dd2d06336674c
Josh Kendrick <jck0004@tigermail.auburn.edu>
2014-02-03 22:42:31 -0600

    Initial Change (Depot App)
    From Agile Devl Book

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '51a4d4415a5ddbb805a2d298ec31a3b47addfb641a2303b7e8394ab1776a7d2ba27ab61a968f8feae2273b1099812b16aeb7cbd260e25e92d64bea872a35562b'
